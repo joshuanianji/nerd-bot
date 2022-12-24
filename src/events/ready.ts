ee7d8e52c1c26b1f1817d61bcffe8e86016423bf
Joshua Ji <joshua.ji@jiwhiz.com>
2022-12-24 19:28:20 +0000

    Add log for how many guilds the bot is in

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -5,7 +5,10 @@ import { log } from './../util/log';
 import chalk from 'chalk';
 
 export const ready = async (client: Client<true>): Promise<void> => {
-    log.info(`${chalk.bold(client.user.tag)} is online! Running in ${chalk.bold.bgCyan(client.config.ENV)} mode.`)
+    // get how many guilds the bot is in
+    const guilds = await client.guilds.fetch();
+
+    log.info(`${chalk.bold(client.user.tag)} is online! Running in ${chalk.bold.bgCyan(client.config.ENV)} mode and serving ${chalk.green(guilds)} servers`)
 
     if (client.application === null) {
         throw new Error('Client did not register in time, please try again');
