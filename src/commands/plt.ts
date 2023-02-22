6657b878d69b22289d6fc57e0468e7a0ec573338
Joshua Ji <joshua.ji@jiwhiz.com>
2023-02-22 20:52:05 +0000

    use line plot in example, and embed image

--- src/commands/plt.ts
+++ src/commands/plt.ts
@@ -4,7 +4,7 @@ import { EmbedBuilder } from '@discordjs/builders';
 import { upsertUser } from '../util/upsertUser';
 import Client from './../client';
 import { AttachmentBuilder, CommandInteraction } from 'discord.js';
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 import { getScore } from '../util/getScore';
 import { getScorePlot } from '../util/getScorePlot';
 
@@ -27,12 +27,24 @@ export const plt: Command = {
             embed.setDescription(`Requested by ${intr.user.toString()}`);
         }
 
+        // check if the user has participated yet (they have a record in the database)
+        const active = await client.prisma.user.findFirst({
+            where: { id: user.id },
+        });
+        if (!active) {
+            embed.setDescription(`No nerd stats for ${user.username} yet!`);
+            intr.reply({ embeds: [embed] });
+            return;
+        }
+
         // first, get user (or create an empty one if they haven't participated yet)
+        // the process of setting an image created via an attachmentBuilder on an embed is kinda weird...
         const pltBuffer = await getScorePlot();
         const file = new AttachmentBuilder(pltBuffer, {
             name: `${user.id}-nerd-score.png`,
             description: `Nerd score plot for ${user.username}`,
         });
+        embed.setImage(`attachment://${user.id}-nerd-score.png`);
 
         intr.reply({ embeds: [embed], files: [file] });
         return;
